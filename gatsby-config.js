1c70621cf78c723296a8e01cd90b08a7f4292a56
Ender Bonnet <enndbo@gmail.com>
2018-08-14 10:46:16 -0300

    config google-analytics-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,14 +31,3 @@ module.exports = {
     },
   ],
 }
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123910567-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-123910567-1');
-</script>
-
